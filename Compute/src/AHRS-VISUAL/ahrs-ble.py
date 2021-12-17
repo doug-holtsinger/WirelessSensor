3b2302e6cb23d7f68cec89593a33afed79b25a16
Doug Holtsinger <dsholt4@hotmail.com>
2021-12-16 18:19:47 -0600

    Use a more generic method to find our device

--- Compute/src/AHRS-VISUAL/ahrs-ble.py
+++ Compute/src/AHRS-VISUAL/ahrs-ble.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
-# from datetime import datetime
-# import binascii
+""" Print the manufacturing data for our Wireless Sensor device 
+    to standard out.  It assumes a manufacturer ID of 0xffff and 
+    a length of exactly 16 bytes for the manufacturing data.
+"""
+
 import sys
 from bluepy.btle import Scanner, DefaultDelegate
 
@@ -11,14 +14,18 @@ class ScanDelegate(DefaultDelegate):
         self.pkt_num = 0
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
-        # print(datetime.now().time(), dev.rawData, dev.addr)
-        # if dev.addr == "cc:43:80:8d:f8:46":
-        if dev.addr == "f1:68:47:7c:ad:e3":
-            manufacturer_data_t = dev.getValueText(255)[4:]
-            roll = manufacturer_data_t[0:4]
-            pitch = manufacturer_data_t[4:8]
-            yaw   = manufacturer_data_t[8:12]
+        try:
+            manufacturer_data = dev.getValueText(255)
+        except BTLEException as e:
+            print(e)
+            sys.exit(1) 
+        if manufacturer_data is not None and len(manufacturer_data) == 16 and manufacturer_data[0:4] == 'ffff': 
+            # print("dev.addr %s data %s len %d" % ( dev.addr , manufacturer_data[0:4] , len(manufacturer_data)))
+            roll = manufacturer_data[0:4]
+            pitch = manufacturer_data[4:8]
+            yaw   = manufacturer_data[8:12]
             print roll + ' ' + pitch + ' ' + yaw + ' ' + str(self.pkt_num)
+            # print dev.rawData
             sys.stdout.flush()
             self.pkt_num = self.pkt_num + 1
 
