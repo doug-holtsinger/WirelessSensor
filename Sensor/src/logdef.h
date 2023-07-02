fd46a039b6fb762f56ab87c77e372dc9e0a088b6
doug-holtsinger <dsholt4@hotmail.com>
2023-07-02 12:11:21 -0500

    Fixed defect in Yaw display when connected

--- Sensor/src/logdef.h
+++ Sensor/src/logdef.h
@@ -8,32 +8,36 @@
 #define __LOGDEF_H__
 
 /* print defines */
+#define PRINTF_FLOAT_FORMATI " %c%ld"
+#define PRINTF_FLOAT_VALUEI(val) (uint8_t)(((val) < 0 && (val) > -1.0) ? '-' : ' '),   \
+                           (int32_t)(val)
+
 #define PRINTF_FLOAT_FORMAT " %c%ld.%01ld"
-#define PRINTF_FLOAT_VALUE(val) (uint8_t)(((val) < 0 && (val) > -1.0) ? '-' : ' '),   \
+#define PRINTF_FLOAT_VALUE(val) (uint8_t)(((val) < 0) ? '-' : ' '),   \
                            (int32_t)(val),                                       \
                            (int32_t)((((val) > 0) ? (val) - (int32_t)(val)       \
                                                 : (int32_t)(val) - (val))*10)
 
 #define PRINTF_FLOAT_FORMAT2 " %c%ld.%02ld"
-#define PRINTF_FLOAT_VALUE2(val) (uint8_t)(((val) < 0 && (val) > -1.0) ? '-' : ' '),   \
+#define PRINTF_FLOAT_VALUE2(val) (uint8_t)(((val) < 0) ? '-' : ' '),   \
                            (int32_t)(val),                                       \
                            (int32_t)((((val) > 0) ? (val) - (int32_t)(val)       \
                                                 : (int32_t)(val) - (val))*100)
 
 #define PRINTF_FLOAT_FORMAT3 " %c%ld.%03ld"
-#define PRINTF_FLOAT_VALUE3(val) (uint8_t)(((val) < 0 && (val) > -1.0) ? '-' : ' '),   \
+#define PRINTF_FLOAT_VALUE3(val) (uint8_t)(((val) < 0) ? '-' : ' '),   \
                            (int32_t)(val),                                       \
                            (int32_t)((((val) > 0) ? (val) - (int32_t)(val)       \
                                                 : (int32_t)(val) - (val))*1000)
 
 #define PRINTF_FLOAT_FORMAT4 " %c%ld.%04ld"
-#define PRINTF_FLOAT_VALUE4(val) (uint8_t)(((val) < 0 && (val) > -1.0) ? '-' : ' '),   \
+#define PRINTF_FLOAT_VALUE4(val) (uint8_t)(((val) < 0) ? '-' : ' '),   \
                            (int32_t)(val),                                       \
                            (int32_t)((((val) > 0) ? (val) - (int32_t)(val)       \
                                                 : (int32_t)(val) - (val))*10000)
 
 #define PRINTF_FLOAT_FORMAT7 " %c%ld.%07ld"
-#define PRINTF_FLOAT_VALUE7(val) (uint8_t)(((val) < 0 && (val) > -1.0) ? '-' : ' '),   \
+#define PRINTF_FLOAT_VALUE7(val) (uint8_t)(((val) < 0) ? '-' : ' '),   \
                            (int32_t)(val),                                       \
                            (int32_t)((((val) > 0) ? (val) - (int32_t)(val)       \
                                                 : (int32_t)(val) - (val))*10000000)
